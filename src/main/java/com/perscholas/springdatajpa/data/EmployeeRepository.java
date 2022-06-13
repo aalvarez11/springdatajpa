c267d26a55d678ad6c59f2dc49fa7428ab00a89d
aalvarez11 <aalvarez1@email.arizona.edu>
2022-06-13 14:03:30 -0700

    the rest of the tutorial that doesn't use Ultimate features

--- src/main/java/com/perscholas/springdatajpa/data/EmployeeRepository.java
+++ src/main/java/com/perscholas/springdatajpa/data/EmployeeRepository.java
@@ -3,5 +3,9 @@ package com.perscholas.springdatajpa.data;
 import com.perscholas.springdatajpa.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findEmployeeByLastNameContaining(String str);
 }
